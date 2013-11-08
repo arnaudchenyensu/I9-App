6a4409d29cdf00d431e0d23b3a3b3689138b7427
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-11-08 17:51:09 +0400

    update view

--- twitter-like/app/views/home.blade.php
+++ twitter-like/app/views/home.blade.php
@@ -5,6 +5,20 @@
 <body>
 
 	{{ Form::open(array('url' => 'user')) }}
+		    {{ Form::label('username', 'Username or email: ') }} 
+		    {{ Form::text('username') }} </br>
+
+		    {{ Form::label('email', 'Email: ') }}
+		    {{ Form::email('email') }} </br>
+
+		    {{ Form::submit('Sign in') }} </br>
+		    
+		    <!-- TODO -->
+			{{ Form::checkbox('remember') }} Remember me - Forgot password
+	{{ Form::close() }}
+	
+	<h3>New to Twitter? Sign up</h3>
+	{{ Form::open(array('url' => 'user')) }}
 		    {{ Form::label('username', 'Username: ') }} 
 		    {{ Form::text('username') }} </br>
 
